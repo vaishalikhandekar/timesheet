93c2f3801feb7ef61892348844245d9078512d50
rahulkhandekar <rahul.khandekar@yahoo.co.in>
2016-09-09 20:49:42 -0500

    Timesheet attributes changed.

--- components/timesheet/data/ddl/AddConstraint.sql
+++ components/timesheet/data/ddl/AddConstraint.sql
@@ -1,9 +1,9 @@
 ALTER TABLE TimeSheetProcess ADD CONSTRAINT TimeSheetProcess PRIMARY KEY(timeSheetProcessID);
 
-ALTER TABLE TimeSheet ADD CONSTRAINT TimeSheet PRIMARY KEY(timeSheetID);
-
-ALTER TABLE TimeSheetLineItem ADD CONSTRAINT TimeSheetLineItem PRIMARY KEY(timeSheetLineItemID);
-
 ALTER TABLE TimeSheetApprovalLevel ADD CONSTRAINT TimeSheetApprovalLevel PRIMARY KEY(timeSheetApprovalLevelID);
 
 ALTER TABLE ProjectTimeSheetProcessLink ADD CONSTRAINT ProjectTimeSheetProcessLink PRIMARY KEY(projectTimeSheetProcessLinkID);
+
+ALTER TABLE TimeSheet ADD CONSTRAINT TimeSheet PRIMARY KEY(timeSheetID);
+
+ALTER TABLE TimeSheetLineItem ADD CONSTRAINT TimeSheetLineItem PRIMARY KEY(timeSheetLineItemID);
--- components/timesheet/data/ddl/CreateTables.sql
+++ components/timesheet/data/ddl/CreateTables.sql
@@ -2,8 +2,29 @@
 
 CREATE TABLE TimeSheetProcess (
 	timeSheetProcessID NUMBER(19,0) NOT NULL,
-	bilable NUMBER(10),
 	frequency VARCHAR2(30),
+	startDay VARCHAR2(20),
+	startDate DATE,
+    endDate DATE,
+	comments VARCHAR2(20),
+	recordStatus VARCHAR2(10),	
+	versionNo NUMBER(10)
+);
+
+CREATE TABLE ProjectTimeSheetProcessLink (
+	projectTimeSheetProcessLinkID NUMBER(19,0) NOT NULL,
+	timeSheetProcessID NUMBER(19,0),
+	projectID NUMBER(19,0),
+	regularHours NUMBER(10),
+	startDate DATE,
+    endDate DATE,
+	comments VARCHAR2(20),
+	recordStatus VARCHAR2(10),	
+	versionNo NUMBER(10)
+);
+
+CREATE TABLE TimeSheetApprovalLevel (
+	timeSheetApprovalLevelID NUMBER(19,0) NOT NULL,
 	startDate DATE,
     endDate DATE,
 	comments VARCHAR2(20),
@@ -16,7 +37,8 @@ CREATE TABLE TimeSheet (
 	projectTimeSheetProcessLinkID NUMBER(19,0),
 	totalRegularHours NUMBER(10),
 	totalActualHours NUMBER(10),
-	submittedDate DATE,
+	createdDateTime TIMESTAMP,
+	submittedDateTime TIMESTAMP,
 	timeSheetStatus VARCHAR2(10),
 	startDate DATE,
     endDate DATE,
@@ -30,29 +52,7 @@ CREATE TABLE TimeSheetLineItem (
 	timeSheetID NUMBER(19,0),
 	category VARCHAR2(20),
 	attendenceDate DATE,
-	actualRegularHoursWorked NUMBER(10),
-	actualOvertimeHoursWorked NUMBER(10),
-	comments VARCHAR2(20),
-	recordStatus VARCHAR2(10),	
-	versionNo NUMBER(10)
-);
-CREATE TABLE TimeSheetApprovalLevel (
-	timeSheetApprovalLevelID NUMBER(19,0) NOT NULL,
-	startDate DATE,
-    endDate DATE,
-	comments VARCHAR2(20),
-	recordStatus VARCHAR2(10),	
-	versionNo NUMBER(10)
-);
-
-CREATE TABLE ProjectTimeSheetProcessLink (
-	projectTimeSheetProcessLinkID NUMBER(19,0) NOT NULL,
-	timeSheetProcessID NUMBER(19,0),
-	projectID NUMBER(19,0),
-	regularHours NUMBER(10),
-	startDay VARCHAR2(20),
-	startDate DATE,
-    endDate DATE,
+	actualHoursWorked NUMBER(10),
 	comments VARCHAR2(20),
 	recordStatus VARCHAR2(10),	
 	versionNo NUMBER(10)
